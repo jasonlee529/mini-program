3c407c4d08ba3e13ee2834ce7a2818b8952b305a
jason <libo529@gmail.com>
2018-11-12 09:50:53 +0800

    update files

--- xiaokele_data/src/App.vue
+++ xiaokele_data/src/App.vue
@@ -1,13 +1,16 @@
 <script>
 export default {
-  created () {
+  created() {
     // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    console.log('app created and cache logs by setStorageSync')
+    const logs = wx.getStorageSync("logs") || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync("logs", logs);
+    console.log("app created and cache logs by setStorageSync");
+  },
+  afterUserInfo(e) {
+    console.log(e);
   }
-}
+};
 </script>
 
 <style>
